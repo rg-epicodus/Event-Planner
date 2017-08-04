a71ebc84aad5b170b3f00d1d97bcb28a80e3cac6
Guest User <Guest@Epicodus-5D.local>
2017-08-04 09:14:21 -0700

    fix first test to be inline with new structure

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -4,6 +4,23 @@ package models;
  * Created by Guest on 8/4/7.
  */
 public class Event {
+    private int numGuests;
+    private String typeFood;
+    private String typeDrinks;
+    private String typeEntertainment;
+    private int totalPrice;
+
+    public Event(int guests, String food, String drinks, String entertainment, int price) {
+        numGuests = guests;
+        typeFood = food;
+        typeDrinks = drinks;
+        typeEntertainment = entertainment;
+        totalPrice = price;
+    }
+
+    public int getGuests() {
+        return numGuests;
+    }
 }
 
 
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -11,7 +11,8 @@ public class EventTest {
 
     @Test
     public void newEvent_instantiatescorrectly_true() {
-        Event testEvent = new Event();
+        Event testEvent = new Event(1, "Snacks", "Water", "Cousin Vinny", 0);
         assertEquals(true, testEvent instanceof Event);
     }
+
 }
