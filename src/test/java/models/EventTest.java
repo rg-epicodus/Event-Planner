f07858b1359456bf43af332c6cf597012454672b
Guest User <Guest@Epicodus-5D.local>
2017-08-04 09:27:22 -0700

    pass test to call getDrinks

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -30,7 +30,7 @@ public class EventTest {
     @Test
     public void getDrinks_getsDrinkType_Water() {
         Event testEvent = new Event(5, "Snacks", "Water", "Cousin Vinny", 0);
-        assertEquals("magma", testEvent.getDrinks());
+        assertEquals("Water", testEvent.getDrinks());
     }
 
 
